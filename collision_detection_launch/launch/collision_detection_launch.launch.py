9c7e3b12450aef210785d555573bb37786ba0f06
au616728@uni.au.dk <au616728@uni.au.dk>
2022-05-17 11:07:36 +0200

    launch cleanup

--- collision_detection_launch/launch/collision_detection_launch.launch.py
+++ collision_detection_launch/launch/collision_detection_launch.launch.py
@@ -25,43 +25,14 @@ def generate_launch_description():
     ur_type='ur5e'
     robot_ip = '127.0.0.1'
     use_fake_hardware='true'
-    controllers_file="controllers.yml"
-    arguments_for_ur_launch={'ur_type':ur_type,
-                            'robot_ip':robot_ip,
-                            'launch_rviz':'false',
-                            'use_fake_hardware':use_fake_hardware,
-                            'initial_joint_controller':'joint_trajectory_controller',
-                            'description_package':'collision_detection_launch',
-                            'runtime_config_package':'collision_detection_launch',
-                            'controllers_file':controllers_file,
-                            'description_file':"shared.urdf.xacro",
-                            'multi_setup':"true"
-    }
-    arguments_for_ur_moveit={'ur_type':ur_type, 'robot_ip':robot_ip, 'launch_rviz':'false','use_fake_hardware':use_fake_hardware}
-    ur_driver_bringup_dir = get_package_share_directory("ur_bringup")
+    arguments_for_moveit={'ur_type':ur_type, 'robot_ip':robot_ip, 'launch_rviz':'false','use_fake_hardware':use_fake_hardware}
     collision_detection_launch_dir = get_package_share_directory("collision_detection_launch")
 
-
-    ur_nodes = GroupAction(
-        actions=[
-            # PushRosNamespace(ur_ns),
-            IncludeLaunchDescription(
-                launch_description_source= PythonLaunchDescriptionSource(ur_driver_bringup_dir+'/launch/ur_control.launch.py'),
-                launch_arguments=arguments_for_ur_launch.items()
-            )
-        ]
-    )
     trajectory_generator = IncludeLaunchDescription(
                 launch_description_source= PythonLaunchDescriptionSource(collision_detection_launch_dir+'/launch/trajectory_generator_ur.launch.py'),
-                launch_arguments=arguments_for_ur_moveit.items()
+                launch_arguments=arguments_for_moveit.items()
             )
 
-    # kuka setup:
-    kuka_joint_trajectory_controller = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=['position_trajectory_controller', "--controller-manager", "/controller_manager"],
-    )
 
     # shared:
     unity_tcp_endpoint = Node(
--- collision_detection_launch/launch/trajectory_generator_ur.launch.py
+++ collision_detection_launch/launch/trajectory_generator_ur.launch.py
@@ -26,50 +26,35 @@ from ur_bringup.launch_common import load_yaml, load_yaml_abs
 def launch_setup(context, *args, **kwargs):
 
     # Initialize Arguments
-    robot_ip = LaunchConfiguration("robot_ip")
     safety_limits = LaunchConfiguration("safety_limits")
     safety_pos_margin = LaunchConfiguration("safety_pos_margin")
     safety_k_position = LaunchConfiguration("safety_k_position")
     # General arguments
-    description_package_ur = LaunchConfiguration("description_package")
-    description_file_ur = LaunchConfiguration("description_file")
-    moveit_config_package_ur = LaunchConfiguration("moveit_config_package")
-    moveit_config_file_ur = LaunchConfiguration("moveit_config_file")
+    description_package = LaunchConfiguration("description_package")
+    description_file = LaunchConfiguration("description_file")
+    moveit_config_package = LaunchConfiguration("moveit_config_package")
+    moveit_config_file = LaunchConfiguration("moveit_config_file")
     prefix = LaunchConfiguration("prefix")
-    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
-    fake_sensor_commands = LaunchConfiguration("fake_sensor_commands")
     headless_mode = LaunchConfiguration("headless_mode")
 
     joint_limit_params = PathJoinSubstitution(
-        [FindPackageShare(description_package_ur), "config", "joint_limits.yaml"]
+        [FindPackageShare(description_package), "config", "joint_limits.yaml"]
     )
     kinematics_params = PathJoinSubstitution(
-        [FindPackageShare(description_package_ur), "config", "default_kinematics.yaml"]
+        [FindPackageShare(description_package), "config", "default_kinematics.yaml"]
     )
     physical_params = PathJoinSubstitution(
-        [FindPackageShare(description_package_ur), "config","physical_parameters.yaml"]
+        [FindPackageShare(description_package), "config","physical_parameters.yaml"]
     )
     visual_params = PathJoinSubstitution(
-        [FindPackageShare(description_package_ur), "config","visual_parameters.yaml"]
-    )
-    script_filename = PathJoinSubstitution(
-        [FindPackageShare("ur_robot_driver"), "resources", "ros_control.urscript"]
-    )
-    input_recipe_filename = PathJoinSubstitution(
-        [FindPackageShare("ur_robot_driver"), "resources", "rtde_input_recipe.txt"]
-    )
-    output_recipe_filename = PathJoinSubstitution(
-        [FindPackageShare("ur_robot_driver"), "resources", "rtde_output_recipe.txt"]
+        [FindPackageShare(description_package), "config","visual_parameters.yaml"]
     )
 
     robot_description_content = Command(
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
-            PathJoinSubstitution([FindPackageShare(description_package_ur), "urdf", description_file_ur]),
-            " ",
-            "robot_ip:=",
-            robot_ip,
+            PathJoinSubstitution([FindPackageShare(description_package), "urdf", description_file]),
             " ",
             "joint_limit_params:=",
             joint_limit_params,
@@ -93,34 +78,17 @@ def launch_setup(context, *args, **kwargs):
             safety_k_position,
             " ",
             "name:=",
-            # Also ur_type parameter could be used but then the planning group names in yaml
-            # configs has to be updated!
             "ur",
-            " ",
-            "script_filename:=",
-            script_filename,
-            " ",
-            "input_recipe_filename:=",
-            input_recipe_filename,
-            " ",
-            "output_recipe_filename:=",
-            output_recipe_filename,
             " ",
             "prefix:=",
             prefix,
-            " ",
-            "use_fake_hardware:=",
-            use_fake_hardware,
-            " ",
-            "fake_sensor_commands:=",
-            fake_sensor_commands,
             " ",
             "headless_mode:=",
             headless_mode,
             " ",
         ]
     )
-    robot_description_ur = {"robot_description": robot_description_content}
+    robot_description = {"robot_description": robot_description_content}
 
     # MoveIt Configuration
     robot_description_semantic_content = Command(
@@ -128,7 +96,7 @@ def launch_setup(context, *args, **kwargs):
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
             PathJoinSubstitution(
-                [FindPackageShare(moveit_config_package_ur), "config","srdf", moveit_config_file_ur]
+                [FindPackageShare(moveit_config_package), "config","srdf", moveit_config_file]
             ),
             " ",
             "name:=",
@@ -141,17 +109,17 @@ def launch_setup(context, *args, **kwargs):
             " ",
         ]
     )
-    robot_description_semantic_ur = {"robot_description_semantic": robot_description_semantic_content}
+    robot_description_semantic = {"robot_description_semantic": robot_description_semantic_content}
 
-    kinematics_yaml_ur = load_yaml("collision_detection_launch", "config/kinematics.yaml")
-    robot_description_kinematics_ur = {"robot_description_kinematics": kinematics_yaml_ur}
+    kinematics_yaml = load_yaml("collision_detection_launch", "config/kinematics.yaml")
+    robot_description_kinematics = {"robot_description_kinematics": kinematics_yaml}
 
-    robot_description_planning_ur = {
+    robot_description_planning = {
         "robot_description_planning": load_yaml_abs(str(joint_limit_params.perform(context)))
     }
 
     # Planning Configuration
-    ompl_planning_pipeline_config_ur = {
+    ompl_planning_pipeline_config = {
         "move_group": {
             "name": "my_group_lol",
             "planning_plugin": "ompl_interface/OMPLPlanner",
@@ -160,52 +128,52 @@ def launch_setup(context, *args, **kwargs):
         }
     }
     
-    ompl_planning_yaml_ur = load_yaml("collision_detection_launch", "config/ompl_planning.yaml")
-    ompl_planning_pipeline_config_ur["move_group"].update(ompl_planning_yaml_ur)
+    ompl_planning_yaml = load_yaml("collision_detection_launch", "config/ompl_planning.yaml")
+    ompl_planning_pipeline_config["move_group"].update(ompl_planning_yaml)
 
     # Trajectory Execution Configuration
-    controllers_yaml_ur = load_yaml("collision_detection_launch", "config/controllers.yaml")
-    moveit_controllers_ur = {
-        "moveit_simple_controller_manager": controllers_yaml_ur,
+    controllers_yaml = load_yaml("collision_detection_launch", "config/controllers.yaml")
+    moveit_controllers = {
+        "moveit_simple_controller_manager": controllers_yaml,
         "moveit_controller_manager": "moveit_simple_controller_manager/MoveItSimpleControllerManager",
     }
 
-    trajectory_execution_ur = {
+    trajectory_execution = {
         "moveit_manage_controllers": False,
         "trajectory_execution.allowed_execution_duration_scaling": 1.2,
         "trajectory_execution.allowed_goal_duration_margin": 0.5,
         "trajectory_execution.allowed_start_tolerance": 0.01,
     }
 
-    planning_scene_monitor_parameters_ur = {
+    planning_scene_monitor_parameters = {
         "publish_planning_scene": True,
         "publish_geometry_updates": True,
         "publish_state_updates": True,
         "publish_transforms_updates": True,
     }
 
-    print(robot_description_ur,
-            robot_description_semantic_ur,
-            robot_description_kinematics_ur,
-            robot_description_planning_ur,
-            ompl_planning_pipeline_config_ur,
-            trajectory_execution_ur,
-            moveit_controllers_ur,
-            planning_scene_monitor_parameters_ur)
+    print(robot_description,
+            robot_description_semantic,
+            robot_description_kinematics,
+            robot_description_planning,
+            ompl_planning_pipeline_config,
+            trajectory_execution,
+            moveit_controllers,
+            planning_scene_monitor_parameters)
         # Start the actual move_group node/action server
     move_group_node = Node(
         package="moveit_ros_move_group",
         executable="move_group",
         output="screen",
         parameters=[
-            robot_description_ur,
-            robot_description_semantic_ur,
-            robot_description_kinematics_ur,
-            robot_description_planning_ur,
-            ompl_planning_pipeline_config_ur,
-            trajectory_execution_ur,
-            moveit_controllers_ur,
-            planning_scene_monitor_parameters_ur,
+            robot_description,
+            robot_description_semantic,
+            robot_description_kinematics,
+            robot_description_planning,
+            ompl_planning_pipeline_config,
+            trajectory_execution,
+            moveit_controllers,
+            planning_scene_monitor_parameters,
         ],
     )
     
@@ -216,14 +184,14 @@ def launch_setup(context, *args, **kwargs):
         executable="trajectory_generator_node",
         output="screen",
         parameters=[
-            robot_description_ur,
-            robot_description_semantic_ur,
-            robot_description_kinematics_ur,
-            robot_description_planning_ur,
-            ompl_planning_pipeline_config_ur,
-            trajectory_execution_ur,
-            moveit_controllers_ur,
-            planning_scene_monitor_parameters_ur,
+            robot_description,
+            robot_description_semantic,
+            robot_description_kinematics,
+            robot_description_planning,
+            ompl_planning_pipeline_config,
+            trajectory_execution,
+            moveit_controllers,
+            planning_scene_monitor_parameters,
         ],
         # arguments=['--ros-args','--log-level','debug']
     )
@@ -245,19 +213,7 @@ def launch_setup(context, *args, **kwargs):
 def generate_launch_description():
 
     declared_arguments = []
-    # UR specific arguments
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "ur_type",
-            description="Type/series of used UR robot.",
-            choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e"],
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "robot_ip", description="IP address by which the robot can be reached."
-        )
-    )
+
     declared_arguments.append(
         DeclareLaunchArgument(
             "safety_limits",
@@ -317,21 +273,6 @@ def generate_launch_description():
             description="Prefix of the joint names, useful for \
         multi-robot setup. If changed than also joint names in the controllers' configuration \
         have to be updated.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "use_fake_hardware",
-            default_value="false",
-            description="Start robot with fake hardware mirroring command to its states.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "fake_sensor_commands",
-            default_value="false",
-            description="Enable fake command interfaces for sensors used for simple simulations. \
-            Used only if 'use_fake_hardware' parameter is true.",
         )
     )
     declared_arguments.append(

c899042ba790ddbc3e0e70b0f778a998b8708b95
au616728@uni.au.dk <au616728@uni.au.dk>
2022-05-21 14:58:38 +0200

    Cleanup

--- collision_detection_launch/launch/collision_detection_launch.launch.py
+++ collision_detection_launch/launch/collision_detection_launch.launch.py
@@ -8,16 +8,10 @@ def generate_launch_description():
 
 
 
-    # ur setup:
-    ur_type='ur5e'
-    robot_ip = '127.0.0.1'
-    use_fake_hardware='true'
-    arguments_for_moveit={'ur_type':ur_type, 'robot_ip':robot_ip, 'launch_rviz':'false','use_fake_hardware':use_fake_hardware}
     collision_detection_launch_dir = get_package_share_directory("collision_detection_launch")
 
     trajectory_generator = IncludeLaunchDescription(
-                launch_description_source= PythonLaunchDescriptionSource(collision_detection_launch_dir+'/launch/trajectory_generator_ur.launch.py'),
-                launch_arguments=arguments_for_moveit.items()
+                launch_description_source= PythonLaunchDescriptionSource(collision_detection_launch_dir+'/launch/trajectory_generator_ur.launch.py')
             )
 
 
@@ -25,7 +19,7 @@ def generate_launch_description():
     unity_tcp_endpoint = Node(
         package='ros_tcp_endpoint',
         executable='default_server_endpoint',
-        name='my_tcp_endpoint',
+        name='unity_tcp_endpoint',
         parameters=[{'ROS_IP':'127.0.0.1'}]
     )
 

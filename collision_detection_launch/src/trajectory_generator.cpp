4a188417064736df21a3a3060c2f4045baf6fad7
au616728@uni.au.dk <au616728@uni.au.dk>
2022-05-25 16:29:57 +0200

    Documentation and refactoring

--- collision_detection_launch/src/trajectory_generator.cpp
+++ collision_detection_launch/src/trajectory_generator.cpp
@@ -5,47 +5,46 @@
 using JointState = sensor_msgs::msg::JointState;
 using JointTrajectory = trajectory_msgs::msg::JointTrajectory;
 TrajectoryGenerator::TrajectoryGenerator()
-    : Node("trajectory_generator")
-    {
-        RCLCPP_INFO(rclcpp::get_logger("trajectory_generator"), "Creating trajectory generator!");
-        service_ =
-        this->create_service<collision_detection_msgs::srv::GenerateTrajectory>("generate_trajectory_srv", std::bind(&TrajectoryGenerator::generate_trajectory,this,std::placeholders::_1,std::placeholders::_2));
-        service1_ =
-        this->create_service<collision_detection_msgs::srv::GenerateTrajectory>("generate_trajectory_srv1", std::bind(&TrajectoryGenerator::generate_trajectory,this,std::placeholders::_1,std::placeholders::_2));
-
-    }
+: Node("trajectory_generator")
+{
+    service_ =
+    this->create_service<collision_detection_msgs::srv::GenerateTrajectory>("generate_trajectory_srv", std::bind(&TrajectoryGenerator::generate_trajectory_service,this,std::placeholders::_1,std::placeholders::_2));
+    service1_ =
+    this->create_service<collision_detection_msgs::srv::GenerateTrajectory>("generate_trajectory_srv1", std::bind(&TrajectoryGenerator::generate_trajectory_service,this,std::placeholders::_1,std::placeholders::_2));
+}
 
 moveit::planning_interface::MoveGroupInterfacePtr TrajectoryGenerator::setupMoveGroup(std::string planning_group)
 {
-    RCLCPP_INFO(rclcpp::get_logger("trajectory_generator"), "trying!");
   auto ret = std::make_shared<moveit::planning_interface::MoveGroupInterface>(shared_from_this(), planning_group);
-  RCLCPP_INFO(rclcpp::get_logger("trajectory_generator"), "Success!");
   return ret;
 }
 
 
-void TrajectoryGenerator::generate_trajectory(const std::shared_ptr<collision_detection_msgs::srv::GenerateTrajectory::Request> request,
-        std::shared_ptr<collision_detection_msgs::srv::GenerateTrajectory::Response>      response){
-        auto move_group = setupMoveGroup(request->move_group);
-        JointTrajectory res;
-        for(int i=0;i<(request->states.size()-1);i++){
-            auto traj = generateTrajectory(request->states[i],request->states[i+1],move_group);
-            addTrajectories(res,traj);
-        }
-        // printTrajectory(robert_plant.trajectory_.joint_trajectory);
-        RCLCPP_INFO_STREAM(get_logger(), "joint trajectory:" << rosidl_generator_traits::to_yaml(res));
-        response->set__res(res);
+void TrajectoryGenerator::generate_trajectory_service(const std::shared_ptr<collision_detection_msgs::srv::GenerateTrajectory::Request> request,
+    std::shared_ptr<collision_detection_msgs::srv::GenerateTrajectory::Response>      response){
+    auto move_group = setupMoveGroup(request->move_group);
+    auto res = generate_and_add_trajectories(request->states,move_group);
+    RCLCPP_INFO_STREAM(get_logger(), "joint trajectory:" << rosidl_generator_traits::to_yaml(res));
+    response->set__res(res);
+}
+JointTrajectory TrajectoryGenerator::generate_and_add_trajectories(std::vector<JointState> states,moveit::planning_interface::MoveGroupInterfacePtr move_group){
+    JointTrajectory res;
+    for(int i=0;i<(states.size()-1);i++)
+    {
+        auto traj = generateTrajectory(states[i],states[i+1],move_group);
+        addTrajectories(res,traj);
+    }
+    return res;
 }
 
 JointTrajectory TrajectoryGenerator::generateTrajectory(JointState a,JointState b,moveit::planning_interface::MoveGroupInterfacePtr move_group){
     moveit_msgs::msg::RobotState start_state;
-        start_state.set__joint_state(a);
-        move_group->setStartState(start_state);
-        move_group->setJointValueTarget(b);
-        moveit::planning_interface::MoveGroupInterface::Plan robert_plant;
-        //plan
-        move_group->plan(robert_plant);
-        return robert_plant.trajectory_.joint_trajectory;
+    start_state.set__joint_state(a);
+    move_group->setStartState(start_state);
+    move_group->setJointValueTarget(b);
+    moveit::planning_interface::MoveGroupInterface::Plan robert_plant;
+    move_group->plan(robert_plant);
+    return robert_plant.trajectory_.joint_trajectory;
 }
 
 void TrajectoryGenerator::addTrajectories(JointTrajectory& a,JointTrajectory& b){

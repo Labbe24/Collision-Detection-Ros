12d9a8897959c3266705037e402a56c285315173
au616728@uni.au.dk <au616728@uni.au.dk>
2022-05-25 16:56:51 +0200

    documented launch file

--- collision_detection_launch/launch/collision_detection_launch.launch.py
+++ collision_detection_launch/launch/collision_detection_launch.launch.py
@@ -4,8 +4,21 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import launch
 
-def generate_launch_description():
+##
+# @file
+# Launch file for spawning the ROS2 nodes used in the trajectory generation module.
+#
 
+## This function generates a launch description for the Trajectory generation module.
+#It includes a tcp-endpoint node, communicating with Unity.
+#A move-group node providing MoveIt2 planning services
+#A trajectory-generator Node providing trajectory generation services.
+def generate_launch_description():
+    ## @function
+    #This function generates a launch description for the Trajectory generation module.
+    #It includes a tcp-endpoint node, communicating with Unity.
+    #A move-group node providing MoveIt2 planning services
+    #A trajectory-generator Node providing trajectory generation services.
 
 
     collision_detection_launch_dir = get_package_share_directory("collision_detection_launch")
